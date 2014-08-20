767201ca0b4b181e0160fa30269c1978d975afe2
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-08-20 17:00:17 +0900

    use absolute path for mysql

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-mysql -u isu-user isucon < /home/isu-user/isucon/config/init.sql > log 2>&1
+/usr/bin/mysql -u isu-user isucon < /home/isu-user/isucon/config/init.sql > log 2>&1
 #データ投入後になにかしらの作業をしたい場合はこのシェルスクリプトに書いてください
 
