3530e39d37a7046c16ee71f2e3f3ac9707947d58
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-08-20 16:45:27 +0900

    force index

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -76,7 +76,7 @@ class Isucon3App < Sinatra::Base
     user  = get_user
 
     total = mysql.query("SELECT count(*) AS c FROM memos WHERE is_private=0").first["c"]
-    memos = mysql.query("SELECT memos.*, users.username FROM memos JOIN users ON users.id = memos.user WHERE memos.is_private=0  ORDER BY memos.created_at DESC, memos.id DESC LIMIT 100")
+    memos = mysql.query("SELECT memos.*, users.username FROM memos FORCE INDEX(memos_idx_id_is_private_created_at_id) JOIN users ON users.id = memos.user WHERE memos.is_private=0  ORDER BY memos.created_at DESC, memos.id DESC LIMIT 100")
     # memos.each do |row|
     #   row["username"] = mysql.xquery("SELECT username FROM users WHERE id=?", row["user"]).first["username"]
     # end
@@ -94,7 +94,7 @@ class Isucon3App < Sinatra::Base
 
     page  = params["page"].to_i
     total = mysql.xquery('SELECT count(*) AS c FROM memos WHERE is_private=0').first["c"]
-    memos = mysql.xquery("SELECT memos.*, users.username FROM memos JOIN users ON users.id = memos.user WHERE memos.is_private=0  ORDER BY memos.created_at DESC, memos.id DESC LIMIT 100 OFFSET #{page * 100}")
+    memos = mysql.xquery("SELECT memos.*, users.username FROM memos FORCE INDEX(memos_idx_id_is_private_created_at_id) JOIN users ON users.id = memos.user WHERE memos.is_private=0  ORDER BY memos.created_at DESC, memos.id DESC LIMIT 100 OFFSET #{page * 100}")
 #    memos = mysql.xquery("SELECT * FROM memos WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100 OFFSET #{page * 100}")
     if memos.count == 0
       halt 404, "404 Not Found"
