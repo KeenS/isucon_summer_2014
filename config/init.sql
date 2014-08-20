3674767ba9a7425aabf0ccb609623972a9f48b87
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-08-20 15:58:02 +0900

    fix syntax error

--- config/init.sql
+++ config/init.sql
@@ -1,3 +1,3 @@
-CREATE INDEX users_idx_username ON users (username)
-CREATE INDEX memos_idx_id_is_private_created_at_id (is_private, created_at, id)
-CREATE INDEX memos_idx_id_user_created_at (`user`, created_at)
+CREATE INDEX users_idx_username ON users (username);
+CREATE INDEX memos_idx_id_is_private_created_at_id ON memos (is_private, created_at, id);
+CREATE INDEX memos_idx_id_user_created_at ON memos (`user`, created_at);
